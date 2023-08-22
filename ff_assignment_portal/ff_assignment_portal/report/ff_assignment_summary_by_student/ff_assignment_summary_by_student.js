dabbc061dbf61828c3c6340effa4db016c29bdfa
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-22 13:18:41 +0530

    feat: colored status

--- ff_assignment_portal/ff_assignment_portal/report/ff_assignment_summary_by_student/ff_assignment_summary_by_student.js
+++ ff_assignment_portal/ff_assignment_portal/report/ff_assignment_summary_by_student/ff_assignment_summary_by_student.js
@@ -3,4 +3,18 @@
 
 frappe.query_reports["FF Assignment Summary By Student"] = {
   filters: [],
+  formatter: function (value, row, column, data, default_formatter) {
+    // if value is "Failed" make it red
+    if (value == "Failed") {
+      value = `<span style="color: red;">${value}</span>`;
+    } else if (value == "Passed") {
+      value = `<span style="color: green;">${value}</span>`;
+    } else if (value == "Unchecked") {
+      value = `<span style="color: orange;">${value}</span>`;
+    } else if (value == "Check In Progress") {
+      value = `<span style="color: blue;">${value}</span>`;
+    }
+    
+    return default_formatter(value, row, column, data);
+  },
 };
