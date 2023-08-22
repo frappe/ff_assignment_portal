17270b2bcf1adce63d1f76c131b6cab8992403f7
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-22 12:10:40 +0530

    chore: add report boilerplate

---
+++ ff_assignment_portal/ff_assignment_portal/report/ff_assignment_summary_by_student/ff_assignment_summary_by_student.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["FF Assignment Summary By Student"] = {
+	"filters": [
+
+	]
+};
